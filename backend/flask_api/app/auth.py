f28febc18e869daa7b94d84f650c378d8ed2e300
Kamil Hebda <153030471+Kamil-Hebda@users.noreply.github.com>
2024-05-24 22:29:36 +0200

    Update auth.py

---
+++ backend/flask_api/app/auth.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, request, jsonify, make_response
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
+# import jwt
+# import datetime
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/auth', methods=['POST'])
+def register():
+    data= request.get_json() # get the data from the request
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    new_user = User(public_id=str(uuid.uuid4()), email=data['email'], password=hashed_password, username=data['username'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'registered successfully'}) # return a message to the user
+
+@auth.route('/login', methods=['POST'])
+def login():
+    auth= request.authorization # get the authorization from the request
+    if not auth or not auth.username or not auth.password:
+        return make_response('could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'}) #401 is the status code for unauthorized
+    
+    user = User.query.filter_by(username=auth.username).first()
+    if not user:
+        return jsonify({'message': 'user not found'})
+    
+    if check_password_hash(user.password, auth.password):
+       
+        return jsonify({'message': 'login successful'})
+    else:
+        return jsonify({'message': 'login failed'})
+    
+
+
+
+    
