dd7d3eb3e6661a956b730b046c4d25c6f1f624a6
JaSycz <kubasy2019@gmail.com>
2024-06-07 03:52:05 +0200

    f

--- frontend/human_benchmark/src/pages/speedClicker/speedClicker.tsx
+++ frontend/human_benchmark/src/pages/speedClicker/speedClicker.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect, useRef} from "react";
 import { gameContainerVariants } from "../../assets/animationVariants";
 import styles from './speedClicker.module.css';
 import { motion } from "framer-motion";
@@ -6,14 +6,36 @@ import { motion } from "framer-motion";
 export default function speedClicker() {
    const [clickerValue, setClickerValue] = useState<number>(0);
    const [isGameOnValue, setIsGameOnValue] = useState<boolean>(false);
-    
+   const intervalRef = useRef<number>();
+   const [now, setNow] = useState<number>();
+   const [startTime, setStartTime] = useState<number>();  
+
     const onClickGameStart = () => {
         setIsGameOnValue(true);
         setClickerValue(1);
+        setStartTime(Date.now());
+        setNow(Date.now());
+
+        clearInterval(intervalRef.current);
+        intervalRef.current = setInterval(() => {
+            setNow(Date.now());
+        }, 10);
 
 
     }
+    const onClickGame = () => {
+        setClickerValue(clickerValue+1);
+    }
+    
+    let secondsPassed = 0;
+    if (startTime != null && now != null) {
+      secondsPassed = (now - startTime) / 1000;
+    }
+    const initialTime = 5;  
 
+
+
+    
     return (
         <div className={styles.speedClicker}>
             <motion.div 
@@ -25,6 +47,7 @@ export default function speedClicker() {
               <div className={styles.statsBlock}>
                   <div className={styles.timeBlock}>
                     <p>Time</p>
+                    {isGameOnValue ? (initialTime - secondsPassed).toFixed(3) : (<></>)}
                   </div>
                   <div className={styles.clicksBlock}>
                     <p>Click/s</p>
@@ -39,7 +62,7 @@ export default function speedClicker() {
               <button  onClick={onClickGameStart}> Click here to start! </button>
               ) : (
               <>
-                <button>  </button>
+                <button onClick={onClickGame}>  </button>
               </>
               )}
               </div>
