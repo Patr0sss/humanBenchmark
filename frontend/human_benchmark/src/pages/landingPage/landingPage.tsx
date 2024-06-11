3fcab97c3cf17977c5c8ee0681db8505dad5ab18
Patr0sss <kkiejno@gmail.com>
2024-06-11 22:59:00 +0200

    added missing game icons + new game name ( TZWCTR(CH) )

--- frontend/human_benchmark/src/pages/landingPage/landingPage.tsx
+++ frontend/human_benchmark/src/pages/landingPage/landingPage.tsx
@@ -1,18 +1,19 @@
 import Duck from "../../assets/duck";
-import Monkey from "../../assets/monkey";
 import Ufo from "../../assets/ufo";
 import WaterLily from "../../assets/waterLily";
 import GameCard from "../../components/gameCard/gameCard";
 import styles from "./landingPage.module.css";
 import Keyboard from "../../assets/keyboard";
+import Clicker from "../../assets/clicker";
+import Treasure from "../../assets/treasure";
 
 const GAMES = [
   { title: "Aim Trainer", icon: <Ufo />, linkURL: "tests/aim" },
   { title: "Sequence", icon: <Duck />, linkURL: "tests/sequence" },
   { title: "Memory", icon: <WaterLily />, linkURL: "tests/memory" },
   { title: "Typing", icon: <Keyboard />, linkURL: "/tests/typing" },
-  { title: "Clicker", icon: <Monkey />, linkURL: "/tests/clicker" },
-  { title: "placeholder", icon: <Monkey />, linkURL: "tests/find" },
+  { title: "Clicker", icon: <Clicker />, linkURL: "/tests/clicker" },
+  { title: "TZWCTR(CH)", icon: <Treasure />, linkURL: "tests/find" },
 ];
 
 export default function LandingPage() {
