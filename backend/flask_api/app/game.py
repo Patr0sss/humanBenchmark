d59540115069384b530d4ad5f80885c9c338daa2
pawel-rus <pawelrus.637@gmail.com>
2024-06-11 21:46:38 +0200

    games endpoints HUUUUUUUUGE update

--- backend/flask_api/app/game.py
+++ backend/flask_api/app/game.py
@@ -1,178 +1,241 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Flask, Blueprint, request, jsonify, render_template
 from flask_login import login_required, current_user
 from flask_cors import CORS
 from . import db
-import datetime
+#import jwt, os
+
+from models import AimTrainers, MemoryGame, SequenceMemory, Typing, Clicker, Placeholder
+from auth_middleware import token_required
 
 game = Blueprint('game', __name__)
 CORS(game, origins="http://localhost:5173", methods=["GET", "POST"])
 
 @game.route('/aim-trainer', methods=['POST'])
-#@login_required
-def aim_trainer():
-    data = request.get_json()
+@token_required
+def aim_trainer(current_user):
+    try:
+        data = request.get_json()
 
-    db.user.update_one(
-        {"public_id": current_user._public_id},
-        {"$push": {# Dodaj nowy dokument AimTrainer do listy aim_trainers użytkownika
-            "aim_trainers": {  
-                "id": str(datetime.datetime.now()),
-                "accuracy": data['accuracy'],
-                "average_time": data['average_time']
-            }
-        }}
-    )
+        if not data['accuracy'] or not data['average_time']:
+            return jsonify({'message': 'Invalid data provided'}), 400
 
-    return jsonify({'message': 'Aim trainer data added successfully'}), 200
+        add_data = AimTrainers.create(current_user["_id"], data['accuracy'], data['average_time'])
 
-@game.route('/memory-game', methods=['POST'])
-@login_required
+        if not add_data:
+            return jsonify({'message': 'Failed to add aim-trainer data'}), 500 #500 server error
+
+        return jsonify({'message': 'Aim trainer data added successfully'}), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to add aim trainer data",
+            "error": str(e)
+        }), 500
+
+@game.route('/aim-trainer', methods=['GET'])
+@token_required
+def get_aim_trainer(current_user):
+    try:
+        aim_trainers = AimTrainers.get_by_user_id(current_user["_id"])
+        return jsonify({
+            'message': 'Aim trainer data retrieved successfully',
+            'data': aim_trainers
+        }), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to retrieve aim trainer data",
+            "error": str(e),
+            "data": None
+        }), 500
+    
+@game.route('/memory-game', methods=['POST']) #score level
+@token_required
 def memory_game():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    db.user.update_one(
-        {"public_id": current_user._public_id},
-        # Używa operatora $push, aby dodać nowy element do tablicy aim_trainers w dokumencie użytkownika.
-        {"$push": {# Dodaj nowy dokument MemoryGame do listy memory_games użytkownika
-            "memory_games": {
-                "id": str(datetime.datetime.now()),
-                "score": data['score'],
-                "level": data["level"]
-            }
-        }}
-    )
+        if not data['score'] or not data['level']:
+            return jsonify({'message': 'Invalid data provided'}), 400
+        
+        add_data = MemoryGame.create(current_user["_id"], data['score'], data['level'])
 
-    return jsonify({'message': 'Memory game data added successfully'}), 200
-
+        if not add_data:
+            return jsonify({'message': 'Failed to add memory-game data'}), 500
+        
+        return jsonify({'message': 'Memory-game data added successfully'}), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to add memory game data",
+            "error": str(e)
+        }), 500
+    
+@game.route('/memory-game', methods=['GET'])
+@token_required
+def get_memory_game(current_user):
+    try:
+        memory_games = MemoryGame.get_by_user_id(current_user["_id"])
+        return jsonify({
+            'message': 'Memory game data retrieved successfully',
+            'data': memory_games
+        }), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to retrieve memory game data",
+            "error": str(e),
+            "data": None
+        }), 500
+    
 @game.route('/sequence-memory', methods=['POST'])
-@login_required
+@token_required
 def sequence_memory():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    db.user.update_one(
-        {"public_id": current_user._public_id},
-        {"$push": {# Dodaj nowy dokument SequenceMemory do listy sequence_memories użytkownika
-            "sequence_memories": {
-                "id": str(datetime.datetime.now()),
-                "score": data['score']
-            }
-        }}
-    )
+        if not data['score']:
+            return jsonify({'message': 'Invalid data provided'}), 400
+        
+        add_data = SequenceMemory.create(current_user["_id"], data['score'])
 
-    return jsonify({'message': 'Sequence memory data added successfully'}), 200
+        if not add_data:
+            return jsonify({'message': 'Failed to add sequence-memory data'}), 500
+        
+        return jsonify({'message': 'Sequence-memory data added successfully'}), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to add sequence memory data",
+            "error": str(e)
+        }), 500
+
+@game.route('/sequence-memory', methods=['GET'])
+@token_required
+def get_sequence_memory(current_user):
+    try:
+        sequence_memories = SequenceMemory.get_by_user_id(current_user["_id"])
+        return jsonify({
+            'message': 'Sequence memory data retrieved successfully',
+            'data': sequence_memories
+        }), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to retrieve sequence memory data",
+            "error": str(e),
+            "data": None
+        }), 500
 
 @game.route('/typing', methods=['POST'])
-@login_required
+@token_required
 def typing():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    db.user.update_one(
-        {"public_id": current_user._public_id},
-        {"$push": {# Dodaj nowy dokument Typing do listy typings użytkownika
-            "typings": {
-                "id": str(datetime.datetime.now()),
-                "score": data['score']
-            }
-        }}
-    )
+        if not data['score']:
+            return jsonify({'message': 'Invalid data provided'}), 400
+        
+        add_data = Typing.create(current_user["_id"], data['score'])
 
-    return jsonify({'message': 'Typing data added successfully'}), 200
+        if not add_data:
+            return jsonify({'message': 'Failed to add typing data'}), 500
+        
+        return jsonify({'message': 'Typing data added successfully'}), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to add typing data",
+            "error": str(e)
+        }), 500
+
+@game.route('/typing', methods=['GET'])
+@token_required
+def get_typing(current_user):
+    try:
+        typings = Typing.get_by_user_id(current_user["_id"])
+        return jsonify({
+            'message': 'Typing data retrieved successfully',
+            'data': typings
+        }), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to retrieve typing data",
+            "error": str(e),
+            "data": None
+        }), 500
 
 @game.route('/clicker', methods=['POST'])
-@login_required
+@token_required
 def clicker():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    db.user.update_one(
-        {"public_id": current_user._public_id},
-        {"$push": {# Dodaj nowy dokument Clicker do listy clickers użytkownika
-            "clickers": {
-                "id": str(datetime.datetime.now()),
-                "clicks_per_second": data['clicks_per_second'],
-                "clicks": data['clicks'],
-            }
-        }}
-    )
+        if not data['clicks_per_second'] or not data['clicks']:
+            return jsonify({'message': 'Invalid data provided'}), 400
+        
+        add_data = Clicker.create(current_user["_id"], data['clicks_per_second'], data['clicks'])
 
-    return jsonify({'message': 'Clicker data added successfully'}), 200
+        if not add_data:
+            return jsonify({'message': 'Failed to add clicker data'}), 500
+        
+        return jsonify({'message': 'Clicker data added successfully'}), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to add clicker data",
+            "error": str(e)
+        }), 500
+
+@game.route('/clicker', methods=['GET'])
+@token_required
+def get_clicker(current_user):
+    try:
+        clickers = Clicker.get_by_user_id(current_user["_id"])
+        return jsonify({
+            'message': 'Clicker data retrieved successfully',
+            'data': clickers
+        }), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to retrieve clicker data",
+            "error": str(e),
+            "data": None
+        }), 500
 
 @game.route('/placeholder', methods=['POST'])
-@login_required
+@token_required
 def placeholder():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    db.user.update_one(
-        {"public_id": current_user._public_id},
-        {"$push": {# Dodaj nowy dokument Placeholder do listy placeholders użytkownika
-            "placeholders": {
-                "id": str(datetime.datetime.now()),
-                "score": data['score']
-            }
-        }}
-    )
+        if not data['score']:
+            return jsonify({'message': 'Invalid data provided'}), 400
+        
+        add_data = Placeholder.create(current_user["_id"], data['score'])
 
-    return jsonify({'message': 'Placeholder data added successfully'}), 200
-
-
-@game.route('/get-aim-trainer', methods=['GET'])
-@login_required
-def get_aim_trainer():
-    user = db.user.find_one({"public_id": current_user._public_id})
-
-    aim_trainers = user['aim_trainers'].find().sort({ "accuracy": -1}).limit(10)
-
-    return jsonify(aim_trainers), 200
-
-@game.route('/get-memory-game', methods=['GET'])
-@login_required
-def get_memory_game():
-    user = db.user.find_one({"public_id": current_user._public_id})
-
-    memory_games = user['memory_games'].find().sort({ "score": -1}).limit(10)
-
-    return jsonify(memory_games), 200
-
-@game.route('/get-sequence-memory', methods=['GET'])
-@login_required
-def get_sequence_memory():
-    user = db.user.find_one({"public_id": current_user._public_id})
-
-    sequence_memories = user['sequence_memories'].find().sort({ "score": -1}).limit(10)
-
-    return jsonify(sequence_memories), 200
-
-@game.route('/get-typing', methods=['GET'])
-@login_required
-def get_typing():
-    user = db.user.find_one({"public_id": current_user._public_id})
-
-    typings = user['typings'].find().sort({ "score": -1}).limit(10)
-
-    if not typings:
-        return jsonify({"message": "No typings data available"}), 404
+        if not add_data:
+            return jsonify({'message': 'Failed to add placeholder data'}), 500
+        
+        return jsonify({'message': 'Placeholder data added successfully'}), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to add placeholder data",
+            "error": str(e)
+        }), 500
     
-    return jsonify(typings), 200
+@game.route('/placeholder', methods=['GET'])
+@token_required
+def get_placeholder(current_user):
+    try:
+        placeholders = Placeholder.get_by_user_id(current_user["_id"])
+        return jsonify({
+            'message': 'Placeholder data retrieved successfully',
+            'data': placeholders
+        }), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Failed to retrieve placeholder data",
+            "error": str(e),
+            "data": None
+        }), 500
 
-@game.route('/get-clicker', methods=['GET'])
-@login_required
-def get_clicker():
-    user = db.user.find_one({"public_id": current_user._public_id})
-
-    clickers = user['clickers'].find().sort({ "clicks_per_second": -1}).limit(10)
-
-    if not clickers:
-        return jsonify({"message": "No clickers data available"}), 404
-    
-    return jsonify(clickers), 200
-
-@game.route('/get-placeholder', methods=['GET'])
-@login_required
-def get_placeholder():
-    user = db.user.find_one({"public_id": current_user._public_id})
-
-    placeholders = user['placeholders'].find().sort({ "score": -1}).limit(10)
-
-    if not placeholders:
-        return jsonify({"message": "No placeholders data available"}), 404
-    
-    return jsonify(placeholders), 200
+@game.errorhandler(404)
+def endpoint_not_found(e):
+    return jsonify({
+        "message": "Endpoint not found",
+        "error": str(e),
+        "data": None
+    }), 404
